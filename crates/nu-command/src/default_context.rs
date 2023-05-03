20bfebb587b1e88ba266ce3ae66bc86fa83fc390
Antoine Stevan <44101798+amtoine@users.noreply.github.com>
2023-05-04 00:02:03 +0200

    REFACTOR: move `source` out of deprecated commands (#9060)
    # Description
    the plan of deprecating `source` never really came to conclusion, so i
    propose to move it out of the deprecated commands in this PR.
    i've moved it to `nu-command::misc`, which can be changed :+1: 
    
    # User-Facing Changes
    ```
    $nothing
    ```
    
    # Tests + Formatting
    - :green_circle: `toolkit fmt`
    - :green_circle: `toolkit clippy`
    - :black_circle: `toolkit test`
    - :black_circle: `toolkit test stdlib`
    
    # After Submitting
    ```
    $nothing
    ```

--- crates/nu-command/src/default_context.rs
+++ crates/nu-command/src/default_context.rs
@@ -103,6 +103,7 @@ pub fn create_default_context() -> EngineState {
 
         // Misc
         bind_command! {
+            Source,
             Tutor,
         };
 
@@ -444,7 +445,6 @@ pub fn create_default_context() -> EngineState {
             MathEvalDeprecated,
             OldAlias,
             RPadDeprecated,
-            Source,
             StrCollectDeprecated,
             StrDatetimeDeprecated,
             StrDecimalDeprecated,
